fdacd7d99186c0bd8405b6b73db8d29b3654d329
Edward Muller <freeformz@gmail.com>
2019-06-23 14:34:11 -0700

    Updates

--- sbin/install.sh
+++ sbin/install.sh
@@ -1,23 +1,60 @@
 #!/usr/bin/env zsh
 
-typeset -A files
+set -e
 
+typeset -A requirements
+
+requirements=(
+  gsed "brew install gnu-sed"
+  gpg "brew install gnupg"
+  git-lfs "brew install git-lfs"
+  go "brew install go"
+  git-codereview "GO111MODULE=off go get -u golang.org/x/review/git-codereview"
+)
+
+need=()
+
+for r in ${(k)requirements}; do
+  if ! type ${r} >>&/dev/null; then
+    need+="${requirements[$r]}"
+  fi
+done
+
+if [[ $#need -gt 0 ]]; then
+  echo "Missing requirements, to resolve run:"
+  for i in ${need}; do
+    echo "\t${i}"
+  done
+  exit 1
+fi
+
+
+typeset -A files
 files=(
   zshrc ~/.zshrc
-  zshrc.work.gpg ~/.zshrc.work
   gitconfig ~/.gitconfig
 )
 
 base=$(git rev-parse --show-toplevel)
 source="${base}/source"
 
-if [ -f ~/.zshrc.work ]; then
-  export EMAIL="emuller@salesforce.com"
-  export KEY="FC5833DB021899A5"
-else
-  export EMAIL="freeformz@gmail.com"
-  export KEY=""
-fi
+[[ $(hostname) =~ "salesforce.com" ]] && situation="work" || situation="home"
+
+case ${situation} in
+  work)
+    files["zshrc.work.gpg"] = "~/.zshrc.work"
+    export EMAIL="emuller@salesforce.com"
+    export KEY="FC5833DB021899A5"
+  ;;
+  home)
+    export EMAIL="freeformz@gmail.com"
+    export KEY="4425D7E25D8A8486"
+  ;;
+  *)
+    echo "unknown situation"
+    exit 1
+  ;;
+esac
 
 for f in ${(k)files}; do
   local tgt=${files[$f]}
